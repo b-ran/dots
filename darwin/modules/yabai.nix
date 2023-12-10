aa73ec62e727749576171aab206a8dc075ca0052
Brandon Scott-Hill <bran.scotthill@gmail.com>
2023-12-10 23:21:09 +1300

    Fixing yabi

--- darwin/modules/yabai.nix
+++ darwin/modules/yabai.nix
@@ -22,36 +22,20 @@ with lib;
       yabai = {
         enable = true;
         package = pkgs.yabai;
-        config = {
+        config = {                  # Extra Config
           layout = "bsp";
           auto_balance = "off";
           split_ratio = "0.50";
           window_border = "on";
           window_border_width = "2";
-          window_border_radius = "11";
-          window_border_hidpi = "off";
-          active_window_opacity = "1.00";
-          normal_window_opacity = "0.95";
-          window_opacity_duration = "0.15";
-          window_topmost = "off";
-          window_shadow = "float";
           window_placement = "second_child";
-          window_animation_duration = "0.22";
           focus_follows_mouse = "autoraise";
-          mouse_modifier = "fn";
-          mouse_follows_focus = "on";
-          mouse_drop_action = "swap";
-          mouse_action1 = "move";
-          mouse_action2 = "resize";
-          active_window_border_color = "0xffe1e3e4";
-          normal_window_border_color = "0xff2a2f38";
-          insert_feedback_color = "0xff9dd274";
-
+          mouse_follows_focus = "off";
           top_padding = "10";
           bottom_padding = "10";
           left_padding = "10";
           right_padding = "10";
-          window_gap = "8";
+          window_gap = "10";
         };
         extraConfig = ''
           yabai -m rule --add app='^Emacs$' manage=on
@@ -62,7 +46,7 @@ with lib;
           yabai -m rule --add app='Activity Monitor' manage=off layer=above
           yabai -m rule --add app='Finder' manage=off layer=above
           yabai -m rule --add app='^System Information$' manage=off layer=above
-        '';
+        '';                         # Window Rules
       };
     };
   };
