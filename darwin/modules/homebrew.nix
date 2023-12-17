e81db2912db1ab1830b84ed4e4c9acab3beb7aba
Brandon Scott-Hill <bran.scotthill@gmail.com>
2023-12-17 20:20:30 +1300

    Refactoring config

--- darwin/modules/homebrew.nix
+++ darwin/modules/homebrew.nix
@@ -11,33 +11,34 @@ with lib;
       '';
     };
   };
-
-  homebrew = {
-    enable = true;
-    onActivation = {
-      autoUpdate = false;
-      upgrade = false;
-      cleanup = "zap";
+  config = mkIf config.homebrew.enable {
+    homebrew = {
+      enable = true;
+      onActivation = {
+        autoUpdate = false;
+        upgrade = false;
+        cleanup = "zap";
+      };
+      taps = [
+        "homebrew/cask-versions"
+      ];
+      brews = [
+      ];
+      casks = [
+        # "alacritty" broken in homebrew
+        "firefox-developer-edition"
+        "google-chrome"
+        "slack"
+        "spotify"
+        "discord"
+        "intellij-idea"
+        "1password"
+        "displaylink"
+        "pritunl"
+        "pika"
+        "keepassxc"
+        "raycast"
+      ];
     };
-    taps = [
-      "homebrew/cask-versions"
-    ];
-    brews = [
-    ];
-    casks = [
-      # "alacritty" broken in homebrew
-      "firefox-developer-edition"
-      "google-chrome"
-      "slack"
-      "spotify"
-      "discord"
-      "intellij-idea"
-      "1password"
-      "displaylink"
-      "pritunl"
-      "pika"
-      "keepassxc"
-      "raycast"
-    ];
   };
 }
--- darwin/modules/system.nix
+++ darwin/modules/system.nix
@@ -1,6 +1,5 @@
 { config, lib, pkgs, ...}:
 
-
 with lib;
 {
   options.system = {
@@ -14,46 +13,48 @@ with lib;
   };
 
   # Changes will require logging out and back in to take effect.
-  system = {
-    defaults = {
-      NSGlobalDomain = {
-        KeyRepeat = 1;
-        NSAutomaticCapitalizationEnabled = false;
-        NSAutomaticSpellingCorrectionEnabled = false;
+  config = mkIf config.system.enable {
+    system = {
+      defaults = {
+        NSGlobalDomain = {
+          KeyRepeat = 1;
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticSpellingCorrectionEnabled = false;
+        };
+        spaces = {
+          spans-displays = false;
+        };
+        screensaver = {
+          askForPassword = true;
+          askForPasswordDelay = 0;
+        };
+        dock = {
+          autohide = true;
+          orientation = "bottom";
+          showhidden = true;
+          static-only = true;
+          mru-spaces = false;
+          tilesize = 40;
+        };
+        finder = {
+          QuitMenuItem = true;
+          AppleShowAllFiles = true;
+          AppleShowAllExtensions = true;
+          ShowPathbar = true;
+          ShowStatusBar = true;
+        };
+        trackpad = {
+          Clicking = true;
+          Dragging = true;
+          TrackpadThreeFingerDrag = true;
+          TrackpadRightClick = true;
+        };
+        loginwindow = {
+          DisableConsoleAccess = true;
+        };
       };
-      spaces = {
-        spans-displays = false;
-      };
-      screensaver = {
-        askForPassword = true;
-        askForPasswordDelay = 0;
-      };
-      dock = {
-        autohide = true;
-        orientation = "bottom";
-        showhidden = true;
-        static-only = true;
-        mru-spaces = false;
-        tilesize = 40;
-      };
-      finder = {
-        QuitMenuItem = true;
-        AppleShowAllFiles = true;
-        AppleShowAllExtensions = true;
-        ShowPathbar = true;
-        ShowStatusBar = true;
-      };
-      trackpad = {
-        Clicking = true;
-        Dragging = true;
-        TrackpadThreeFingerDrag = true;
-        TrackpadRightClick = true;
-      };
-      loginwindow = {
-        DisableConsoleAccess = true;
-      };
+      activationScripts.postActivation.text = ''sudo chsh -s ${pkgs.zsh}/bin/zsh'';
+      stateVersion = 4;
     };
-    activationScripts.postActivation.text = ''sudo chsh -s ${pkgs.zsh}/bin/zsh'';
-    stateVersion = 4;
   };
 }
