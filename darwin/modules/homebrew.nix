c3dc8d72ca0b30068ac8273c1ae484643d5af44f
Brandon Scott-Hill <bran.scotthill@gmail.com>
2023-12-17 20:22:14 +1300

    Refactoring config

--- darwin/modules/homebrew.nix
+++ darwin/modules/homebrew.nix
@@ -2,43 +2,31 @@
 
 with lib;
 {
-  options.homebrew = {
-    enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = mdDoc ''
-        Homebrew package manager for macOS with several packages and casks.
-      '';
+  homebrew = {
+    onActivation = {
+      autoUpdate = false;
+      upgrade = false;
+      cleanup = "zap";
     };
-  };
-  config = mkIf config.homebrew.enable {
-    homebrew = {
-      enable = true;
-      onActivation = {
-        autoUpdate = false;
-        upgrade = false;
-        cleanup = "zap";
-      };
-      taps = [
-        "homebrew/cask-versions"
-      ];
-      brews = [
-      ];
-      casks = [
-        # "alacritty" broken in homebrew
-        "firefox-developer-edition"
-        "google-chrome"
-        "slack"
-        "spotify"
-        "discord"
-        "intellij-idea"
-        "1password"
-        "displaylink"
-        "pritunl"
-        "pika"
-        "keepassxc"
-        "raycast"
-      ];
-    };
+    taps = [
+      "homebrew/cask-versions"
+    ];
+    brews = [
+    ];
+    casks = [
+      # "alacritty" broken in homebrew
+      "firefox-developer-edition"
+      "google-chrome"
+      "slack"
+      "spotify"
+      "discord"
+      "intellij-idea"
+      "1password"
+      "displaylink"
+      "pritunl"
+      "pika"
+      "keepassxc"
+      "raycast"
+    ];
   };
 }
