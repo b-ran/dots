ddfcd3b9f70a5ab581508bc60701f85a47078883
Brandon Scott-Hill <bran.scotthill@gmail.com>
2023-12-17 21:06:17 +1300

    Refactoring config

--- darwin/home.nix
+++ darwin/home.nix
@@ -1,22 +1,8 @@
 { lib, pkgs, user, ... }:
 
 {
-#      imports =
-#        [( import ../home/programs/cli/bat.nix )] ++
-#        [( import ../home/programs/cli/btop.nix )] ++
-#        [( import ../home/programs/cli/exa.nix )] ++
-#        [( import ../home/programs/cli/fzf.nix )] ++
-#        [( import ../home/programs/cli/git.nix )] ++
-#        [( import ../home/programs/cli/htop.nix )] ++
-#        [( import ../home/programs/cli/lazygit.nix )] ++
-#        [( import ../home/programs/cli/ripgrep.nix )] ++
-#        [( import ../home/programs/cli/starship.nix )] ++
-#        [( import ../home/programs/cli/tmux.nix )] ++
-#        [( import ../home/programs/cli/zsh.nix )] ++
-#        [( import ../home/programs/apps/alacritty.nix )];
-
     imports =
-      (import ../../home/programs/apps) ++
+      (import ../home/programs/apps) ++
       (import ../home/programs/cli);
 
     programs = {
