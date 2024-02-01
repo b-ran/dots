70bd74aae03a99f30df3fbd16df76eb6cbb54b90
Brandon Scott-Hill <bran.scotthill@gmail.com>
2024-02-01 21:51:22 +1300

    Yabai improvments

--- darwin/modules/yabai.nix
+++ darwin/modules/yabai.nix
@@ -31,22 +31,32 @@ with lib;
           window_border_width = "2";
           window_placement = "second_child";
           focus_follows_mouse = "autoraise";
-          mouse_follows_focus = "off";
           top_padding = "10";
           bottom_padding = "10";
           left_padding = "10";
           right_padding = "10";
           window_gap = "10";
+          mouse_follows_focus = "off";
+          mouse_modifier = "fn";
+          mouse_action1 = "move";
+          mouse_action2 = "resize";
         };
         extraConfig = ''
           yabai -m rule --add title='Preferences' manage=off layer=above
+          yabai -m rule --add title='^System Settings$' manage=off layer=above
+          yabai -m rule --add title='^About' manage=off layer=above
           yabai -m rule --add title='^(Opening)' manage=off layer=above
           yabai -m rule --add title='Library' manage=off layer=above
           yabai -m rule --add app='^System Preferences$' manage=off layer=above
           yabai -m rule --add app='Activity Monitor' manage=off layer=above
           yabai -m rule --add app='Finder' manage=off layer=above
           yabai -m rule --add app='^System Information$' manage=off layer=above
+
           yabai -m rule --add app='Pika' manage=off layer=above
+          yabai -m rule --add app='1Password' manage=off layer=above
+
+          yabai -m rule --add app="IntelliJ IDEA" manage=off
+          yabai -m rule --add app="IntelliJ IDEA" title=".*\[(.*)\].*" manage=on
         '';
       };
     };
