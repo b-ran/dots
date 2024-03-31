f9c6f3dce74d1f3f00df7beb2e5a1f59a9807526
Brandon Scott-Hill <bran.scotthill@gmail.com>
2024-03-31 17:56:23 +1300

    Remove dusnt theme for pywal

--- home/services/dunst.nix
+++ home/services/dunst.nix
@@ -1,61 +1,64 @@
+{ config, lib, pkgs, user, ... }:
+
 {
-  services = {
-    dunst = {
-      enable = true;
-      settings = {
-        global = {
-          origin = "top-right";
-          follow = "mouse";
-          font = "JetBrainsMono Nerd Font 12";
-          offset = "15x15";
-          line_height = 10;
-          separator_height = 0;
-          gap_size = 10;
-          frame_width = 2;
-          corner_radius = 10;
-          format = "<b>%s</b>\n%b";
-          alignment = "left";
-          vertical_alignment = "center";
-          show_age_threshold = 60;
-          ellipsize = "middle";
-          ignore_newline = "no";
-          stack_duplicates = true;
-          hide_duplicate_count = true;
-          show_indicators = "no";
 
-          enable_recursive_icon_lookup = true;
-          icon_theme = "Papirus-Dark";
-          min_icon_size = 32;
-          max_icon_size = 48;
+  home.file.".config/dunst/dunstrc.d/99-colors-dunst.conf" = {
+    source =  config.lib.file.mkOutOfStoreSymlink "/home/${user}/.cache/wal/colors-dunst.conf";
+  };
 
-          mouse_left_click = "close";
-          mouse_middle_click = "close_all";
-          mouse_right_click = "do_action, close_current";
+  systemd.user.services.dunst = lib.mkIf config.services.dunst.enable {
+    Service.ExecStart = lib.mkForce "${pkgs.dunst}/bin/dunst";
+  };
 
-          frame_color = "#89DCEB";
-          highlight = "#89DCEB";
-          progress_bar_corner_radius = 5;
-          separator_color = "rgba(0,0,0,0)";
-        };
-        experimental = {
-          per_monitor_dpi = true;
-        };
-        urgency_low = {
-          timeout = 5;
-          background = "#1E1E2E";
-          foreground = "#CDD6F4";
-        };
-        urgency_normal = {
-          timeout = 5;
-          background = "#1E1E2E";
-          foreground = "#CDD6F4";
-        };
-        urgency_critical = {
-          timeout = 5;
-          background = "#1E1E2E";
-          foreground = "#CDD6F4";
-          frame_color = "#FAB387";
-        };
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+
+        browser = "${pkgs.firefox-devedition-bin}/bin/firefox-developer-edition ";
+        dmenu = "${pkgs.dmenu}/bin/dmenu -p dunst";
+
+        origin = "top-right";
+        follow = "mouse";
+        font = "JetBrainsMono Nerd Font 12";
+        offset = "15x15";
+        line_height = 10;
+        separator_height = 0;
+        gap_size = 10;
+        frame_width = 2;
+        corner_radius = 10;
+        format = "<b>%s</b> %b";
+        alignment = "left";
+        vertical_alignment = "center";
+        show_age_threshold = 60;
+        ellipsize = "middle";
+        ignore_newline = "no";
+        stack_duplicates = true;
+        hide_duplicate_count = true;
+        show_indicators = "no";
+
+        enable_recursive_icon_lookup = true;
+        icon_theme = "Papirus-Dark";
+        min_icon_size = 32;
+        max_icon_size = 48;
+
+        mouse_left_click = "close_current";
+        mouse_middle_click = "close_all";
+        mouse_right_click = "do_action, open_url, close_current";
+
+        progress_bar_corner_radius = 5;
+      };
+      experimental = {
+        per_monitor_dpi = true;
+      };
+      urgency_low = {
+        timeout = 5;
+      };
+      urgency_normal = {
+        timeout = 5;
+      };
+      urgency_critical = {
+        timeout = 5;
       };
     };
   };
