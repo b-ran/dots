3e7d8895a678e850b8c219444d365c9f6b87a070
Brandon Scott-Hill <bran.scotthill@gmail.com>
2024-03-31 17:53:56 +1300

    Remove starship theme

--- home/programs/cli/starship.nix
+++ home/programs/cli/starship.nix
@@ -2,26 +2,15 @@
 
 {
   programs.starship =
-    let
-      flavour = "mocha";
-    in
     {
+      enable = true;
       enableZshIntegration = true;
       settings = {
         # Other config here
         format = "$all"; # Remove this line to disable the default prompt format
-        palette = "catppuccin_${flavour}";
-
         aws = {
           disabled = true;
         };
-      } // builtins.fromTOML (builtins.readFile
-        (pkgs.fetchFromGitHub
-          {
-            owner = "catppuccin";
-            repo = "starship";
-            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f"; # Replace with the latest commit hash
-            sha256 = "nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
-          } + /palettes/${flavour}.toml));
-    };
+      };
+  };
 }
