c7d395dd9643911d5b44c38746a1ab072a75dc21
Brandon Scott-Hill <bran.scotthill@gmail.com>
2024-03-31 17:48:43 +1300

    Add cachix

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,7 +2,18 @@
 
 {
   nix = {
-    settings.auto-optimise-store = true;
+    settings = {
+      auto-optimise-store = true;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+
+    };
     gc = {
       automatic = true;
       dates = "weekly";
