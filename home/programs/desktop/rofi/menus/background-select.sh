67354b21d991383b23c86141d4f0d67f055e7b18
Brandon Scott-Hill <bran.scotthill@gmail.com>
2024-03-31 17:50:47 +1300

    Add background selection menu

---
+++ home/programs/desktop/rofi/menus/background-select.sh
@@ -0,0 +1,63 @@
+# Background Path
+backgroundDir="$HOME/wallpapers"
+themesDir="/home/brandon/workspace/nixos-config/home/programs/desktop/rofi/themes"
+
+# Transition config
+TYPE="wipe"
+DURATION=3
+SWWW_PARAMS="--transition-type ${TYPE} --transition-duration ${DURATION}"
+
+# Retrieve image files as a list
+PICS=($(find -L "${backgroundDir}" -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png \)))
+
+# Rofi command
+rofiCommand="rofi -show -dmenu -theme ${themesDir}/select.rasi"
+
+
+executeCommand() {
+  cp -f "$1" ~/.config/current-wallpaper
+  swww img "$1" ${SWWW_PARAMS}
+}
+
+# Show the images
+menu() {
+  for i in "${!PICS[@]}"; do
+    printf "$(basename "${PICS[$i]}" | cut -d. -f1)\x00icon\x1f${PICS[$i]}\n"
+  done
+}
+
+# Execution
+main() {
+  choice=$(menu | ${rofiCommand})
+
+  # No choice case
+  if [[ -z $choice ]]; then
+    exit 0
+  fi
+
+  # Find the selected file
+  for file in "${PICS[@]}"; do
+  # Getting the file
+    if [[ "$(basename "$file" | cut -d. -f1)" = "$choice" ]]; then
+      selectedFile="$file"
+      break
+    fi
+  done
+
+  # Check the file and execute
+  if [[ -n "$selectedFile" ]]; then
+    executeCommand "${selectedFile}"
+    return 0
+  else
+    echo "Image not found."
+    exit 1
+  fi
+}
+
+# Check if rofi is already running
+if pidof rofi > /dev/null; then
+  pkill rofi
+  exit 0
+fi
+
+main
