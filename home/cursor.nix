c3323eed33b211044aa5cf884eab07af01b98d1d
Brandon Scott-Hill <bran.scotthill@gmail.com>
2024-04-21 01:06:29 +1200

    Update cursor theme

--- home/cursor.nix
+++ home/cursor.nix
@@ -2,8 +2,8 @@
 
 {
   home.pointerCursor = {
-    name = "Catppuccin-Mocha-Dark-Cursors";
-    package = pkgs.catppuccin-cursors.mochaDark;
+    name = "BreezeX-RosePine-Linux";
+    package = pkgs.rose-pine-cursor;
     size = 32; # 24, 32, 48, 64
     gtk.enable = true;
     x11.enable = true;
