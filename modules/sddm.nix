eb065b647c72907052b78c41b2205ae153866925
Brandon Scott-Hill <bran.scotthill@gmail.com>
2024-04-21 01:07:32 +1200

    Update sddm theme

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -1,27 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, user, ... }:
 
-let
-  catppuccinSddm = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "sddm";
-    rev = "bde6932e1ae0f8fdda76eff5c81ea8d3b7d653c0";
-    sha256 = "sha256-ceaK/I5lhFz6c+UafQyQVJIzzPxjmsscBgj8130D4dE=";
-  };
-in
 {
-  services.xserver = {
-    enable = true;
-    displayManager = {
-      sessionPackages = [ pkgs.hyprland ];
-      sddm = {
-        enable = true;
-        theme = "catppuccin-mocha";
-        settings = {
-          Theme = {
-            ThemeDir = "${catppuccinSddm}/src/";
-          };
-        };
+  environment.systemPackages = [
+    (pkgs.sddm-chili-theme.override {
+      themeConfig = {
+        background = "/etc/current-wallpaper";
+        backgroundMode = "none";
+        blur = true;
+        recursiveBlurLoops = 5;
+        recursiveBlurRadius = 15;
       };
+    })
+  ];
+  
+  services.xserver.enable = true;
+
+  services.displayManager = {
+    enable = true;
+    sessionPackages = [ pkgs.hyprland ];
+    sddm = {
+      enable = true;
+      theme = "chili";
     };
   };
 }
